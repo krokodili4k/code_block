8d420a9dcd1937c87ffd4b2cf58bb591291aaac8
YuryTruhin <yury.remagon@gmail.com>
2026-02-16 14:18:27 +0700

    vers.1.1

--- code_block/js/script1.js
+++ code_block/js/script1.js
@@ -1,7 +1,39 @@
+// <!-- Переменные -->
+let Variables = [];
 
-function Var(name, value) // <!-- Блок перезаписи переменных -->
+// <!-- Конструкторы -->
+function Variable(name, value)
+{
+    this.name = name
+    this.value = value
+}
+
+// <!-- Функции -->
+function EmptyVars() // <!-- Блок объявления переменных -->
 {   
+    var Vars = (document.getElementById("block_of_variables").value).split(",")
+    for(i in Vars)
+    {
+        Variables.push(new Variable(Vars[i], 0))
+    }
+    console.log(Variables)
+}
 
+
+
+function AddValue()
+{
+    var Name = document.getElementById("textarea_block_varible").value
+    var Value = parseInt(document.getElementById("textarea_block_values").value)
+    for(i in Variables)
+    {
+        if (Name === Variables[i].name)
+        {
+            Variables[i].value = Value
+            break
+        }
+    }
+    console.log(Variables)
 }
 
 function Arifmetic(operation, a, b) // <!-- Арифметика  -->
@@ -17,6 +49,8 @@ function Arifmetic(operation, a, b) // <!-- Арифметика  -->
         case "/":
             
         case "%":
+
+        case "=":
             
     }
 }
