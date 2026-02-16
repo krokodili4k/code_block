22b85faacd84c82216132e072f55a658815988ce
YuryTruhin <yury.remagon@gmail.com>
2026-02-16 22:08:03 +0700

    Ver 1.2 test

--- code_block/js/script1.js
+++ code_block/js/script1.js
@@ -8,10 +8,50 @@ function Variable(name, value)
     this.value = value
 }
 
-// <!-- Функции -->
+// <!-- Вспомогательные функции -->
+function GetVar(a)
+{
+    for(i in Variables)
+        {
+            if (a === Variables[i].name)
+            {
+                return Variables[i]
+        
+            }
+        }
+}
+
+function FindIndOfVarByName(a)
+{
+    for(i in Variables)
+    {
+        if (a === Variables[i].name)
+        {
+            return i 
+        }
+    } 
+}
+
+function FindIndOfVarByVar(a)
+{
+    for(i in Variables)
+    {
+        if (a === Variables[i])
+        {
+            return i 
+        }
+    } 
+}
+
+function ListOfVars()
+{
+    console.log(Variables)
+}
+
+// <!-- Основные функции -->
 function EmptyVars() // <!-- Блок объявления переменных -->
 {   
-    var Vars = (document.getElementById("block_of_variables").value).split(",")
+    var Vars = (document.getElementById("input_block_varible").value).split(",")
     for(i in Vars)
     {
         Variables.push(new Variable(Vars[i], 0))
@@ -36,39 +76,57 @@ function AddValue()
     console.log(Variables)
 }
 
-function Arifmetic(operation, a, b) // <!-- Арифметика  -->
+function Arifmetic() // <!-- Проостая арифметика  -->
 {
+    var First = document.getElementById("input1_arif_block").value
+    var Second = document.getElementById("input2_arifm_block").value
+    var operation = document.getElementById("zaebalo_arifm_block").value
+    First = Variables[FindIndOfVarByName(First)]
+    Second = Variables[FindIndOfVarByName(Second)] 
     switch(operation)
     {
         case "+":
-            
+            console.log(First.value + Second.value)
+            return First.value + Second.value
         case "-":
-            
+            console.log(First.value - Second.value)
+            return First.value - Second.value
         case "*":
-            
+            console.log(First.value * Second.value)
+            return First.value * Second.value
         case "/":
-            
+            console.log(First.value / Second.value)
+            return First.value / Second.value
         case "%":
-
+            console.log(First.value % Second.value)
+            return First.value % Second.value
         case "=":
-            
+            SecondValue = Variables[FindIndOfVarByVar(Second)].value
+            Variables[FindIndOfVarByVar(First)].value = SecondValue
+            console.log(Variables)
     }
+  
 }
 
 function If(operation, a, b)
 {
+    var First = document.getElementById("varible1_condition_block").value
+    var Second = document.getElementById("varible2_condition_block").value
+    var operation = document.getElementById("selector_condition_block").value
+    First = Variables[FindIndOfVarByName(First)]
+    Second = Variables[FindIndOfVarByName(Second)]
     switch(operation)
     {
         case ">":
-
+            return First.value > Second.value
         case "<":
-            
+            return First.value < Second.value
         case "==":
-            
+            return First.value === Second.value
         case ">=":
-            
+            return First.value >= Second.value
         case "<=":
-
+            return First.value <= Second.value
     }
 }
 
@@ -76,3 +134,5 @@ function While()
 {
 
 }
+
+
