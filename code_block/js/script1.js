5281b6d819242db2171cd31a9d3ec8fb9e41122d
YuryTruhin <yury.remagon@gmail.com>
2026-02-16 11:59:14 +0700

    Add files via upload

---
+++ code_block/js/script1.js
@@ -0,0 +1,44 @@
+
+function Var(name, value) // <!-- Блок перезаписи переменных -->
+{   
+
+}
+
+function Arifmetic(operation, a, b) // <!-- Арифметика  -->
+{
+    switch(operation)
+    {
+        case "+":
+            
+        case "-":
+            
+        case "*":
+            
+        case "/":
+            
+        case "%":
+            
+    }
+}
+
+function If(operation, a, b)
+{
+    switch(operation)
+    {
+        case ">":
+
+        case "<":
+            
+        case "==":
+            
+        case ">=":
+            
+        case "<=":
+
+    }
+}
+
+function While()
+{
+
+}
