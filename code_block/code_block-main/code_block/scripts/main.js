64ee757dcdc3658b9e027179395d26b6578956bf
Denis <160056263+krokodili4k@users.noreply.github.com>
2026-03-14 04:41:55 +0700

    Delete code_block/code_block-main directory

--- code_block/code_block-main/code_block/scripts/main.js
+++
@@ -1,5 +0,0 @@
-import { initDragDrop, makeBlockDraggable, addDeleteButton } from "./site_program/drag_drop.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-    initDragDrop('proramingSpace'); 
-});
--- code_block/code_block-main/code_block/scripts/program_back/Storage.js
+++
@@ -1,5 +0,0 @@
-export default class Storage{
-    constructor(){
-        variables: this.variables = {};
-    }
-}
